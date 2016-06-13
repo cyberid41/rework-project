3741fbc613a6171426e57b3fc5cf516f061eb47b
cyberid41 <edicyber@gmail.com>
2016-06-13 13:02:42 +0700

    fix things

--- bots/scrapWeworkRemotely.js
+++ bots/scrapWeworkRemotely.js
@@ -50,110 +50,117 @@ request(baseUrl, function (error, response, html) {
             var bot = new Category(category);
             bot.save(function (error) {
                 if (!error) {
-                    // run get jobs
-                    Category.find({web: baseUrl}, function (err, docs) {
-                        docs.forEach(function (entry) {
-
-                            request(entry.link, function (error, response, html) {
-                                if (!error) {
-                                    var $ = cheerio.load(html);
-
-                                    var job = {
-                                        title: '',
-                                        link: '',
-                                        company: '',
-                                        date: '',
-                                        new: '',
-                                        category: {
-                                            title: entry.title,
-                                            link: entry.link
-                                        },
-                                        web: entry.web
-                                    };
-
-                                    //#category-5 > article > ul > li.feature > a
-                                    $('.jobs-container > section > article > ul > li > a').filter(function () {
-                                        job.link = baseUrl + $(this).attr('href');
-
-                                        var today = new Date();
-                                        var year = today.getFullYear();
-
-                                        // #category-6 > article > ul > li:nth-child(1) > a > span.company
-                                        job.company = $('ul > li:nth-child(1) > a > span.company').text();
-                                        job.title = $('ul > li:nth-child(1) > a > span.title').text();
-                                        job.date = year + ' ' + $('ul > li:nth-child(1) > a > span.date').text();
-                                        // #category-6 > article > ul > li:nth-child(3) > span
-                                        job.new = $('article > ul > li:nth-child(3) > span').text();
-
-                                        var bot = new Job(job);
-                                        bot.save(function (error) {
-                                            if (!error) {
-                                                // run get Detail
-                                                Job.find({}, function (err, docs) {
-                                                    docs.forEach(function (entry) {
-                                                        request(entry.link, function (error, response, html) {
-                                                            if (!error) {
-                                                                var $ = cheerio.load(html);
-
-                                                                var detail = {
-                                                                    title: entry.title,
-                                                                    link: entry.link,
-                                                                    location: '',
-                                                                    body: '',
-                                                                    company: '',
-                                                                    new: entry.new,
-                                                                    date: entry.date,
-                                                                    apply: '',
-                                                                    web: entry.web,
-                                                                    category: {
-                                                                        title: entry.category.title,
-                                                                        link: entry.category.link
-                                                                    }
-                                                                };
-
-                                                                // body > div.container > div.content > div.listing-header > div.listing-header-container > h2 > span.location
-                                                                $('div.listing-header-container > h2').filter(function () {
-
-                                                                    // body > div.container > div.content > div.listing-header > div.listing-header-container > h2 > span.company
-                                                                    detail.company = $('div.listing-header-container > h2 > span.company').text();
-                                                                    // body > div.container > div.content > div.listing-header > div.listing-header-container > h2 > span.location
-                                                                    detail.location = $('div.listing-header-container > h2 > span.location').text();
-                                                                    // body > div.container > div.content > div.listing-header > div.listing-header-container > h2 > a
-                                                                    detail.web = $('div.listing-header-container > h2 > a').attr('href');
-                                                                    // body > div.container > div.content > section > div.listing-container
-                                                                    detail.body = $('div.listing-container').text();
-                                                                    // body > div.container > div.content > div.apply > p
-                                                                    detail.apply = $('div.apply > p').text();
-
-                                                                    var bot = new JobDetail(detail);
-                                                                    bot.save(function (error) {
-                                                                        if (!error) {
-                                                                            console.log('DB Saved job Detail!')
-                                                                        } else {
-                                                                            console.log('Something wrong! with message '.error)
-                                                                        }
-                                                                    });
-                                                                });
-                                                            }
-
-                                                        });
-                                                    });
-                                                });
-                                            } else {
-                                                console.log('Something wrong! with message '.error)
-                                            }
-                                        });
-                                    });
-                                }
-
-                            });
-                        });
-                    });
-
+                    console.log('Category Saved!')
                 } else {
                     console.log('Something wrong! with message '.error)
                 }
             });
+        });
+
+        // run get jobs
+        Category.find({web: baseUrl}, function (err, docs) {
+            docs.forEach(function (entry) {
+
+                request(entry.link, function (error, response, html) {
+                    if (!error) {
+                        var $ = cheerio.load(html);
+
+                        var job = {
+                            title: '',
+                            link: '',
+                            company: '',
+                            date: '',
+                            new: '',
+                            category: {
+                                title: entry.title,
+                                link: entry.link
+                            },
+                            web: entry.web
+                        };
+
+                        //#category-5 > article > ul > li.feature > a
+                        $('.jobs-container > section > article > ul > li > a').filter(function () {
+                            job.link = baseUrl + $(this).attr('href');
+
+                            var today = new Date();
+                            var year = today.getFullYear();
+
+                            // #category-6 > article > ul > li:nth-child(1) > a > span.company
+                            job.company = $('ul > li:nth-child(1) > a > span.company').text();
+                            job.title = $('ul > li:nth-child(1) > a > span.title').text();
+                            job.date = year + ' ' + $('ul > li:nth-child(1) > a > span.date').text();
+                            // #category-6 > article > ul > li:nth-child(3) > span
+                            job.new = $('article > ul > li:nth-child(3) > span').text();
+
+                            var bot = new Job(job);
+                            bot.save(function (error) {
+                                if (!error) {
+
+                                } else {
+                                    console.log('Something wrong! with message '.error)
+                                }
+                            });
+                        });
+                    }
+
+                });
+            });
+
+            console.log('Jobs Saved!')
+        });
+
+        // run get Detail
+        Job.find({}, function (err, docs) {
+            docs.forEach(function (entry) {
+                request(entry.link, function (error, response, html) {
+                    if (!error) {
+                        var $ = cheerio.load(html);
+
+                        var detail = {
+                            title: entry.title,
+                            link: entry.link,
+                            location: '',
+                            body: '',
+                            company: '',
+                            new: entry.new,
+                            date: entry.date,
+                            apply: '',
+                            web: entry.web,
+                            category: {
+                                title: entry.category.title,
+                                link: entry.category.link
+                            }
+                        };
+
+                        // body > div.container > div.content > div.listing-header > div.listing-header-container > h2 > span.location
+                        $('div.listing-header-container > h2').filter(function () {
+
+                            // body > div.container > div.content > div.listing-header > div.listing-header-container > h2 > span.company
+                            detail.company = $('div.listing-header-container > h2 > span.company').text();
+                            // body > div.container > div.content > div.listing-header > div.listing-header-container > h2 > span.location
+                            detail.location = $('div.listing-header-container > h2 > span.location').text();
+                            // body > div.container > div.content > div.listing-header > div.listing-header-container > h2 > a
+                            detail.web = $('div.listing-header-container > h2 > a').attr('href');
+                            // body > div.container > div.content > section > div.listing-container
+                            detail.body = $('div.listing-container').text();
+                            // body > div.container > div.content > div.apply > p
+                            detail.apply = $('div.apply > p').text();
+
+                            var bot = new JobDetail(detail);
+                            bot.save(function (error) {
+                                if (!error) {
+                                    console.log('DB Saved job Detail!')
+                                } else {
+                                    console.log('Something wrong! with message '.error)
+                                }
+                            });
+                        });
+                    }
+
+                });
+            });
+
+            console.log('Job Details Saved!')
         });
     }
 });
